182f8eab9d329d8ab654ba404768b8a505ba2cdf
dmitrii <onolite@gmail.com>
2020-04-25 16:48:27 +0000

    Homework 1

--- core/src/ru/geekbrains/stargame/StarGame.java
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -8,18 +8,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
+	SpriteBatch batchWallpaper;
+	Texture imgWallpaprer;
 	Texture img;
 	
 	@Override
 	public void create () {
+		batchWallpaper = new SpriteBatch();
+		imgWallpaprer = new Texture("grass.jpg");
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
+
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batchWallpaper.begin();
+		batchWallpaper.draw(imgWallpaprer,0,0);
+		batchWallpaper.end();
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
